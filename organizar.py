b3035cf4b63c98ebcf09f9ba7171c1b4848852d5
JOÃ1 <110570660+joa1r@users.noreply.github.com>
2023-08-16 13:35:02 -0300

    Add files via upload

---
+++ organizar.py
@@ -0,0 +1,10 @@
+import os
+import shutil
+
+pasta_inicial = r'C:\Users\Acer\Documents\.Aulas python\PRO_1-4_C111_AtividadeDaProfessora2-main\downloads'
+
+pasta_final = r'C:\Users\Acer\Documents\.Aulas python\PRO_1-4_C111_AtividadeDaProfessora2-main\imagens'
+
+
+lista_de_arquivos = os.listdir(pasta_inicial)
+print(lista_de_arquivos)
